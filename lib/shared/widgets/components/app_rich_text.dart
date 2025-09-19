1884aff0b1f69ad589fab5dcf6736d5da942f4cb
NinhLuongVL <vlnvluon@mediba.jp>
2025-09-19 11:08:36 +0700

    refactor(app_rich_text): replace RichText with Wrap for better layout handling

--- lib/shared/widgets/components/app_rich_text.dart
+++ lib/shared/widgets/components/app_rich_text.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:magic_rewards/config/styles/app_colors.dart';
 import 'package:magic_rewards/shared/extensions/theme_extensions/text_theme_extension.dart';
@@ -8,24 +7,37 @@ class AppRichText extends StatelessWidget {
   final String buttonText;
   final VoidCallback onTap;
 
-  const AppRichText(
-      {super.key,
-      required this.text,
-      required this.buttonText,
-      required this.onTap});
+  const AppRichText({
+    super.key,
+    required this.text,
+    required this.buttonText,
+    required this.onTap,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return RichText(
-      text: TextSpan(
-          text: text,
-          children: [
-            TextSpan(
-                text: "  $buttonText",
-                style: context.f12700,
-                recognizer: TapGestureRecognizer()..onTap = onTap),
-          ],
-          style: context.f12400?.copyWith(color: AppColors.lightGrey)),
+    return Wrap(
+      crossAxisAlignment: WrapCrossAlignment.center,
+      children: [
+        Text(
+          text,
+          style: context.f12400?.copyWith(color: AppColors.lightGrey),
+        ),
+        const SizedBox(width: 4),
+        GestureDetector(
+          onTap: onTap,
+          child: Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 4,
+              vertical: 2,
+            ),
+            child: Text(
+              buttonText,
+              style: context.f12700,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
